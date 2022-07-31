d1802a55ddae537c9eefcd8599075dd76ffd2ba6
LeoLian <lianliang2014@126.com>
2022-07-31 22:16:07 +0800

    commit code.

---
+++ study/01_the_way_to_go/15_chapter/template_validation.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"text/template"
+)
+
+func main() {
+	tOk := template.New("ok")
+
+	template.Must(tOk.Parse("/* and a comment */ some static text: {{ .Name }}"))
+	fmt.Println("the first one parsed OK.")
+	fmt.Println("The next one ought to fail.")
+
+	tErr := template.New("error_template")
+	template.Must(tErr.Parse(" some static text {{.Name}"))
+}
