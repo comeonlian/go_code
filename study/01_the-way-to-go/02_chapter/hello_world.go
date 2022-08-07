c6da103d7829fe1f1e152f33766ef427bae77991
LeoLian <lianliang2014@126.com>
2022-08-07 09:51:41 +0800

    commit code.

---
+++ study/01_the-way-to-go/02_chapter/hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+const val = 12
+
+func main() {
+	fmt.Println("Hello", "world")
+	hehe := 123
+	fmt.Println(hehe)
+}
---
+++ study/01_the-way-to-go/04_chapter/trans/init.go
@@ -0,0 +1,10 @@
+package trans
+
+import "math"
+
+var PI float64
+
+func init()  {
+	PI = 4 * math.Atan(1)
+}
+
