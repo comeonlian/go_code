cc2f3f2fa997203625252ed71ca27ff41546730a
LeoLian <lianliang2014@126.com>
2022-08-13 21:08:39 +0800

    commit code.

--- study/02_data-structure/03_chapter/link_queue_demo.go
+++ study/02_data-structure/03_chapter/link_queue_demo.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go_code/study/02_data-structure/structure"
+)
 
+func main() {
+	queue := new(structure.LinkQueue)
+
+	queue.Push("cat")
+	queue.Push("dee")
+	queue.Push("lili")
+
+	fmt.Println(queue.Pop())
+
+	queue.Push("pipi")
+	queue.Push("fifi")
+
+	fmt.Println(queue.Pop())
 }
--- study/02_data-structure/structure/link_queue.go
+++ study/02_data-structure/structure/link_queue.go
@@ -17,16 +17,34 @@ func (queue *LinkQueue) Push(v string) {
 	queue.lock.Lock()
 	defer queue.lock.Unlock()
 
+	node := new(LinkQueueNode)
+	node.value = v
+
 	if queue.root == nil {
-		queue.root = new(LinkQueueNode)
-		queue.root.value = v
+		queue.root = node
 	} else {
-		
+		curNode := queue.root
+		for curNode.next != nil {
+			curNode = curNode.next
+		}
+		curNode.next = node
 	}
 
 	queue.size += 1
 }
 
 func (queue *LinkQueue) Pop() (v string) {
+	if queue.size == 0 || queue.root == nil{
+		return
+	}
 
+	queue.lock.Lock()
+	defer queue.lock.Unlock()
+
+	curNode := queue.root
+	nextNode := curNode.next
+	queue.root = nextNode
+
+	v = curNode.value
+	return
 }
