21e0121ce962da970f4d49dca1b20a149cfa91c4
LeoLian <lianliang2014@126.com>
2022-08-14 19:56:31 +0800

    commit code.

--- study/02_data-structure/03_chapter/binary_tree_demo.go
+++ study/02_data-structure/03_chapter/binary_tree_demo.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"go_code/study/02_data-structure/structure"
+)
+
+func main() {
+	root := &structure.TreeNode{Data: "A"}
+	root.Left = &structure.TreeNode{Data: "B"}
+	root.Right = &structure.TreeNode{Data: "C"}
+
+	root.Left.Left = &structure.TreeNode{Data: "D"}
+	root.Left.Right = &structure.TreeNode{Data: "E"}
+
+	root.Right.Right = &structure.TreeNode{Data: "F"}
+
+	fmt.Println("先序遍历: ")
+	structure.PreOrder(root)
+	fmt.Println()
+
+	fmt.Println("中序遍历: ")
+	structure.MidOrder(root)
+	fmt.Println()
+
+	fmt.Println("后续遍历: ")
+	structure.PostOrder(root)
+	fmt.Println()
+}
--- study/02_data-structure/structure/tree_node.go
+++ study/02_data-structure/structure/tree_node.go
@@ -1,7 +1,51 @@
 package structure
 
+import "fmt"
+
+// TreeNode 二叉树结构定义
 type TreeNode struct {
 	Left, Right *TreeNode
-	Data string
+	Data        string
 }
 
+// PreOrder 先序遍历
+func PreOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+
+	// 打印根节点
+	fmt.Printf("%v ", node.Data)
+	// 打印左子树
+	PreOrder(node.Left)
+	// 打印右子树
+	PreOrder(node.Right)
+}
+
+// MidOrder 中序遍历
+func MidOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+
+	// 打印左子树
+	MidOrder(node.Left)
+	// 打印根节点
+	fmt.Printf("%v ", node.Data)
+	// 打印右子树
+	MidOrder(node.Right)
+}
+
+// PostOrder 后序遍历
+func PostOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+
+	// 打印左子树
+	PostOrder(node.Left)
+	// 打印右子树
+	PostOrder(node.Right)
+	// 打印根节点
+	fmt.Printf("%v ", node.Data)
+}
